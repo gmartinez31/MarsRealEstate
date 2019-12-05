2f71498af9ddae08753d7863bfd3fca0d712dd5b
gmartinez31 <bhgustavo13@yahoo.com>
2019-12-05 15:17:37 -0700

    Introduced Retrofit and created MarsApiService

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -15,6 +15,50 @@
  *
  */
 
+
+// this file holds the network layer for the app; that is, this is the API the VM will use to communicate with the web service.
+
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = " https://android-kotlin-fun-mars-server.appspot.com/"
+
+
+/*
+ * Retrofit needs at least 2 things available to it to build a web services API:
+ * (1) base URI and (2) a converter factory.
+ * The converter tells Retrofit what do with the data it gets back from the web service.
+ * In this case, we fetch a JSON response from the web service and return it as a String.
+ * Retrofit has a ScalarsConverter that supports strings and other primitive types, so we call
+ * addConverterFactory() on the builder with an instance of ScalarsConverterFactory.
+ */
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+/*
+ * When this method is invoked, Retrofit appends the endpoint 'realestate' to the BASE_URL,
+ * and creates a Call object. That Call object is used to start the HTTP request.
+ */
+interface MarsApiService {
+    @GET("realestate") // <-- path/endpoint
+    fun getProperties(): Call<String>
+}
+
+
+/*
+ * The Retrofit create() method creates the Retrofit service itself with the MarsApiService interface.
+ * Since this call is expensive, and the app only needs one Retrofit service instance, we expose the
+ * service to the rest of the app using a public object called MarsApi and lazily initialize the Retrofit service there.
+ * Each time the app calls MarsApi.retrofitService, it will get a singleton Retrofit object that implements MarsApiService.
+ */
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
