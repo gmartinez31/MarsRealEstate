dbe38718fda775e90ad21077ca5344c151b0ab40
gmartinez31 <bhgustavo13@yahoo.com>
2019-12-09 14:31:02 -0700

    Overview fragment to detail fragment complete

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,15 +17,18 @@
 
 package com.example.android.marsrealestate.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
 // To use variable names in your data class that differ from the key names in the JSON response, use the @Json annotation
 
+@Parcelize
 data class MarsProperty(
         val id: String,
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
-        val price: Double) {
+        val price: Double) : Parcelable {
     val isRental
         get() = type == "rent"
 }
